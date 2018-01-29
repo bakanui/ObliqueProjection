ab65ff6e8cc4560dda8015a7d9dbec64e1fa41ba
Vera Debora Vitamas <veradebora@outlook.co.id>
2018-01-29 19:05:34 +0700

    Trying to initialize cube

--- ProgrammingAssignment2/Form1.vb
+++ ProgrammingAssignment2/Form1.vb
@@ -2,10 +2,15 @@
     Dim canvas As Graphics
     Dim phi As Double = 45 * (Math.PI / 180)
     Dim theta As Double = 45 * (Math.PI / 180)
-    Dim cube(8) As ColMatrix
+    Dim vertex(8) As ColMatrix
+    Dim edges(12) As Edge
     Dim view(4) As ColMatrix
     Dim screen(4) As ColMatrix
     Dim VR(), VS() As ColMatrix
+    Structure Edge
+        Dim point1 As Point
+        Dim point2 As Point
+    End Structure
     Structure ColMatrix
         Dim a, b, c, d As Double
     End Structure
@@ -19,14 +24,14 @@
         canvas = PictureBox1.CreateGraphics()
     End Sub
     Sub Init()
-        SetColMatrix(cube(0), -1, -1, -1, 1)
-        SetColMatrix(cube(1), -1, 1, -1, 1)
-        SetColMatrix(cube(2), 1, -1, -1, 1)
-        SetColMatrix(cube(3), 1, 1, -1, 1)
-        SetColMatrix(cube(4), -1, -1, 1, 1)
-        SetColMatrix(cube(5), -1, 1, 1, 1)
-        SetColMatrix(cube(6), 1, -1, 1, 1)
-        SetColMatrix(cube(7), 1, 1, -1, 1)
+        SetColMatrix(vertex(0), -1, -1, -1, 1)
+        SetColMatrix(vertex(1), -1, 1, -1, 1)
+        SetColMatrix(vertex(2), 1, -1, -1, 1)
+        SetColMatrix(vertex(3), 1, 1, -1, 1)
+        SetColMatrix(vertex(4), -1, -1, 1, 1)
+        SetColMatrix(vertex(5), -1, 1, 1, 1)
+        SetColMatrix(vertex(6), 1, -1, 1, 1)
+        SetColMatrix(vertex(7), 1, 1, -1, 1)
 
         SetColMatrix(view(0), 1, 0, 0, 0)
         SetColMatrix(view(1), 0, 1, 0, 0)
@@ -40,10 +45,24 @@
 
         For i = 0 To 7
             For j = 0 To 3
-                MultiplyMatrix(VR(i), cube(i), view(j))
+                MultiplyMatrix(VR(i), vertex(i), view(j))
                 MultiplyMatrix(VS(i), VR(i), screen(j))
             Next
         Next
+
+        SetEdge(edges(0), VS(0), VS(1))
+        SetEdge(edges(1), VS(1), VS(2))
+        SetEdge(edges(2), VS(2), VS(3))
+        SetEdge(edges(3), VS(3), VS(4))
+        SetEdge(edges(4), VS(4), VS(5))
+        SetEdge(edges(5), VS(5), VS(6))
+        SetEdge(edges(6), VS(7), VS(8))
+        SetEdge(edges(7), VS(9), VS(10))
+        SetEdge(edges(8), VS(11), VS(12))
+        SetEdge(edges(9), VS(0), VS(1))
+        SetEdge(edges(10), VS(0), VS(1))
+        SetEdge(edges(11), VS(0), VS(1))
+
     End Sub
     Sub SetColMatrix(ColMatrix As ColMatrix, a As Double, b As Double, c As Double, d As Double)
         ColMatrix.a = a
@@ -56,5 +75,24 @@
         result.b = MatrixA.b * MatrixB.a
         result.c = MatrixA.c * MatrixB.a
         result.d = MatrixA.d * MatrixB.a
+
+        result.a = result.a + (MatrixA.a * MatrixB.b)
+        result.b = result.b + (MatrixA.b * MatrixB.b)
+        result.c = result.c + (MatrixA.c * MatrixB.b)
+        result.d = result.d + (MatrixA.d * MatrixB.b)
+
+        result.a = result.a + (MatrixA.a * MatrixB.c)
+        result.b = result.b + (MatrixA.b * MatrixB.c)
+        result.c = result.c + (MatrixA.c * MatrixB.c)
+        result.d = result.d + (MatrixA.d * MatrixB.c)
+
+        result.a = result.a + (MatrixA.a * MatrixB.d)
+        result.b = result.b + (MatrixA.b * MatrixB.d)
+        result.c = result.c + (MatrixA.c * MatrixB.d)
+        result.d = result.d + (MatrixA.d * MatrixB.d)
+    End Sub
+    Sub SetEdge(edge As Edge, vertexA As ColMatrix, vertexB As ColMatrix)
+        edge.point1 = New Point(vertexA.a, vertexA.b)
+        edge.point2 = New Point(vertexB.a, vertexB.b)
     End Sub
 End Class
